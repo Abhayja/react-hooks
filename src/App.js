00afed18c5abb2657dcb73fd0f9dfcbb85afa554
Abhayja <91181562+Abhayja@users.noreply.github.com>
2021-10-27 17:44:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState} from "react";
 
-function App() {
+
+
+export default function App() {
+  let [count,setCount] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>React Hooks{count}</div>
+      <button
+      onClick={()=>{setCount(count+1);}} >Click ME</button>
+      
+              </div>
   );
 }
 
-export default App;
+
+
+
